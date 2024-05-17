5ea85b9cdc1c5ac067decf27c854fa5902fde241
Muhammad Munif <160575716+munifff@users.noreply.github.com>
2024-05-17 09:52:53 +0700

    Create Main.java

---
+++ Tugas 9/src/Analisis Percobaan/Main.java
@@ -0,0 +1,10 @@
+//no 6
+package AnalisisPercobaan;
+
+public class Main {
+    public static void main(String[] args) {
+        Manusia manusia = new Manusia("Muhammad Munif Hamdani", 19);
+        manusia.tampilkanNama();
+        manusia.tampilkanUmur();
+    }
+}
